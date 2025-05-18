7980f7bd7f1f937014f105afdb73be5aece8030c
asus-wsl <nguyenhnal@gmail.com>
2025-05-18 09:51:36 +0700

    changes for v0.1.2

--- mq_verify_email.go
+++ mq_verify_email.go
@@ -1,5 +1,15 @@
-package main
+package sharedpayload
 
+// Payload Verify Email
 type PayloadVerifyEmail struct {
 	Email string `json:"email"`
+}
+
+// Payload step upload file
+type PayloadStepUploadFile struct {
+	Object_id      string `json:"object_id"`
+	Uploader_email string `json:"uploader_email"`
+	Folder_id      string `json:"folder_id"`
+	Original_name  string `json:"original_name"`
+	Storage_path   string `json:"storage_path"`
 }
