72c649351205ee2b43dea86d9a95cce6993b37a4
asus-wsl <nguyenhnal@gmail.com>
2025-06-13 17:11:53 +0700

    v0.3.2: add pb core:create tmp obj

---
+++ collect/create_tmp_object.go
@@ -0,0 +1,17 @@
+package collect_svc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/2landhnal/digital-preservation-shared-payload/pb"
+)
+
+func (client *CollectServicerClient) CreateTmpObject(ctx context.Context, req *pb.CreateTmpObjectRequest) (*pb.CreateTmpObjectResponse, error) {
+	// Call the Identify RPC
+	resp, err := client.Client.CreateTmpObject(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("could not call CreateTmpObject GAPI: %v", err)
+	}
+	return resp, nil
+}
