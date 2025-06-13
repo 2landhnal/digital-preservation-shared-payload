a0b8c33a55f79d36882081f8e229ccd2abca874f
asus-wsl <nguyenhnal@gmail.com>
2025-06-13 16:57:24 +0700

    v0.3.0: add context

--- core_svc/upload_file.go
+++ core_svc/upload_file.go
@@ -7,8 +7,8 @@ import (
 	"github.com/2landhnal/digital-preservation-shared-payload/pb"
 )
 
-func (coreServiceClient *CoreServiceClient) UploadFile(req *pb.UploadFileRequest) (*pb.UploadFileResponse, error) {
-	resp, err := coreServiceClient.Client.UploadFile(context.Background(), req)
+func (coreServiceClient *CoreServiceClient) UploadFile(ctx context.Context, req *pb.UploadFileRequest) (*pb.UploadFileResponse, error) {
+	resp, err := coreServiceClient.Client.UploadFile(ctx, req)
 	if err != nil {
 		return nil, fmt.Errorf("could not call UploadFile GAPI: %v", err)
 	}
