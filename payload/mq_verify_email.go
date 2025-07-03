cce2295cc4eecce156e2fa363505d7bb70c28e98
asus-wsl <nguyenhnal@gmail.com>
2025-07-03 21:31:10 +0700

    v0.3.6: add method for payloa

--- payload/mq_verify_email.go
+++ payload/mq_verify_email.go
@@ -22,3 +22,16 @@ type UpLoadFilePayload struct {
 	StoragePath   string         `json:"storage_path" binding:"required"`
 	Pipeline      []PipelineStep `json:"pipeline" binding:"required"` // Pipeline steps to process the file
 }
+
+func (payload *UpLoadFilePayload) NumberOfStep() int {
+	return len(payload.Pipeline)
+}
+
+func (payload *UpLoadFilePayload) PopFirstPipelineStep() *PipelineStep {
+	if len(payload.Pipeline) == 0 {
+		return nil
+	}
+	step := payload.Pipeline[0]
+	payload.Pipeline = payload.Pipeline[1:]
+	return &step
+}
